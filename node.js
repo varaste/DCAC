b2403bb62217e3fd2843876f666d2046a659add0
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-08-11 12:56:02 +0430

    Update node.js

--- node.js
+++ node.js
@@ -17,3 +17,25 @@ function e()
 	  c('Error') 
 	} 
 }
+function digital_clock()
+{
+	var date=new Date();
+	var hours=date.getHours();
+	var minutes=date.getMinutes();
+	var seconds=date.getSeconds();
+	hours=addZero(hours);
+	minutes=addZero(minutes);
+	seconds=addZero(seconds);
+	document.getElementById('hours').innerHTML = hours;
+	document.getElementById('minutes').innerHTML = minutes;
+	document.getElementById('seconds').innerHTML = seconds;
+	setTimeout('digital_clock()',500);
+}
+function addZero(date_part)
+{
+ 	if(date_part < 10)
+		{
+  			date_part="0" + date_part;
+                 }
+		 return date_part;
+}
